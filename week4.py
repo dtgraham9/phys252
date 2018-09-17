bdc19807219a44f4e307c4a5e149e2e74dc88c82
dtgraham9 <dtgraham9@gmail.com>
2018-09-17 11:23:01 -0500

    week4 added

---
+++ week4.py
@@ -0,0 +1,10 @@
+import math
+import numpy
+import scipy.integrate as integrate
+
+k = 8.99*(10**9)
+cm = 10**-2
+micro = 10**-6
+electronCharge = 1.602*10**-19
+electronMass = 9.11*10**-31
+epi = 8.85 * 10**-12
