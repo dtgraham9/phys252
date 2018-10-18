fca41300ad05e63aa07443040aa9d858f628a4d2
Graham Thompson <dtgraham9@gmail.com>
2018-10-17 22:27:13 -0500

    Week7 done

--- week7.py
+++ week7.py
@@ -76,37 +76,85 @@ Rb= .044
 V = Vi*(Rl+Rb)/Rl
 print("Problem 4 part 1: " + str(V))
 V2 = Rl * Af
-
-print("Problem 4 part 2: " + str(Am))
+Vsp = Af*Vi/Ai 
+I = (V-Vsp)/Rb - Af
+print("Problem 4 part 2: " + str(I))
 
 #problem 5
-print("Problem 5: " + str())
+Rs = 19.6
+R2 = 26
+R1 = 10
+Rx = (Rs*R2)/R1
+print("Problem 5: " + str(Rx))
 
 #problem 6 
-print("problem 6: " + str())
+t1 = .5
+V = 95
+VL = 73
+t = t1/math.log(1/(1-VL/V),math.e)
+F = .19 * 10**-6
+R6 = t/F
 
-#problem 7 
-print("Problem 7: " + str())
+print("problem 6: " + str(R6))
+
 
 #problem 8
-print("Problem 8: " + str())
+V = 110
+I = 20
+P = V *I
+W = 300
+L = P/W
+
+print("Problem 8: " + str(L))
 
 #problem 9
-print("Problem 9 part 1: "+str())
-print("Problem 9 part 2: "+ str())
-print("Problem 9 part 3: "+ str()) 
-print("Problem 9 part 4: "+str())
+R = 494.5
+C = 4*10**-6
+Vb = 2.05
+t = 2/1000
+T = R * C
+Vc = Vb*math.e**(-t/T)
+print("Problem 9 part 2: "+ str(Vc))
+Q = Vb * C
+Edis = Vb*(Q/2)
+print("Problem 9 part 3: "+ str(Edis)) 
+Vc = Vb * (1-math.e**(-t/T))
+print("Problem 9 part 4: "+str(Vc))
 
 #problem 10
-print("problem 10 part 1: " +str())
-print("problem 10 part 2: " + str())
-print("Problem 10 part 3: "+ str())
-print("problem 10 part 4: " + str())
+R = 935
+C = 81*10**-6
+T = R *C
+V = 27
+print("problem 10 part 1: " +str(T))
+t = 1.11*10**-1
+q = C * V* (1-math.e**(-t/T))
+print("problem 10 part 2: " + str(q))
+I = V/R * (math.e**(-t/T))
+print("Problem 10 part 3: "+ str(I))
+Vf = V*(1-math.e**(-t/T))
+print("problem 10 part 4: " + str(Vf))
 
 #problem 11
-print("Problem 11 part 1: " + str())
-print("Problem 11 part 2: "+ str()) 
-print("Problme 11 part 3: " + str())
+V = 12
+R = 1.36 * 10**6
+C = 1.89*10**-6
+T = R *C
+print("Problem 11 part 1: " + str(T))
+Q = C * V
+print("Problem 11 part 2: "+ str(Q)) 
+Q = 16.9*10**-6
+t = T* math.log(1/(1-Q/(C*V)),math.e)
+print("Problme 11 part 3: " + str(t))
 
 #problem 12
-print("Problem 12: " + str())
+R1 = 10.1*10**3
+R2 = 16.9*10**3
+C = .42*10**-6
+V = 19
+t = 4.3/1000
+R = R1+R2
+T = R2*C
+i =V/R
+i2 = i*math.e**(-t/T)
+print("Problem 12: " + str(i2))
